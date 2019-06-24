4a58944eb5427672869067ec2ae2d2fc4e436f5d
Magefan <ihoronesuch@gmail.com>
2019-06-24 18:07:58 +0300

    Imporved plugins & toolbars

--- Plugin/Magento/Ui/Component/Wysiwyg/ConfigPlugin.php
+++ Plugin/Magento/Ui/Component/Wysiwyg/ConfigPlugin.php
@@ -43,24 +43,28 @@ class ConfigPlugin
         // Is the current wysiwyg tinymce v4?
         if(strpos($editor,'tinymce4Adapter')){
 
-	        if (($result->getDataByPath('settings/menubar')) || ($result->getDataByPath('settings/toolbar')) || ($result->getDataByPath('settings/plugins'))){
-	            // do not override ui_element config (unsure if this is needed)
-	            return $result;
-	        }
+            if (($result->getDataByPath('settings/menubar')) || ($result->getDataByPath('settings/toolbar')) || ($result->getDataByPath('settings/plugins'))){
+                // do not override ui_element config (unsure if this is needed)
+                return $result;
+            }
 
-	        $settings = $result->getData('settings');
+            $settings = $result->getData('settings');
 
-	        if (!is_array($settings)) {
-	            $settings = [];
-	        }
+            if (!is_array($settings)) {
+                $settings = [];
+            }
 
-	        // configure tinymce settings 
-	        $settings['menubar'] = true;
-	        $settings['toolbar'] = 'undo redo | styleselect | fontsizeselect | forecolor backcolor | bold italic underline strikethrough | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | table | image | code';
-	        $settings['plugins'] = 'textcolor image code';
+            // configure tinymce settings 
+            $settings['menubar'] = true;
+            $settings['image_advtab'] = true;
 
-	        $result->setData('settings', $settings);
-	        return $result;
+            $settings['plugins'] = 'advlist autolink code colorpicker directionality hr imagetools link media noneditable paste print table textcolor toc visualchars anchor charmap codesample contextmenu fullpage help image insertdatetime lists nonbreaking pagebreak preview searchreplace template textpattern visualblocks wordcount magentovariable magentowidget';
+
+            $settings['toolbar1'] = 'magentovariable magentowidget | formatselect | styleselect | fontsizeselect | forecolor backcolor | bold italic underline strikethrough | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent';
+            $settings['toolbar2'] = ' undo redo  | link anchor table charmap | image media insertdatetime | widget | searchreplace visualblocks  help | hr pagebreak';
+
+            $result->setData('settings', $settings);
+            return $result;
         } else{ // don't make any changes if the current wysiwyg editor is not tinymce 4
             return $result;
         }
