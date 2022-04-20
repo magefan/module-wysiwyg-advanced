942d5036ab4d5780f2eb02ac965595696c560a09
Magefan <ihoronesuch@gmail.com>
2022-04-20 09:45:45 +0300

    Added TinyMCE5 support

--- Plugin/Magento/Ui/Component/Wysiwyg/ConfigPlugin.php
+++ Plugin/Magento/Ui/Component/Wysiwyg/ConfigPlugin.php
@@ -94,8 +94,8 @@ class ConfigPlugin
         // Get current wysiwyg adapter's path
         $editor = $this->activeEditor->getWysiwygAdapterPath();
 
-        // Is the current wysiwyg tinymce v4?
-        if (strpos($editor, 'tinymce4Adapter')) {
+        // Is the current wysiwyg tinymce v4 or v5?
+        if (strpos($editor, 'tinymce4Adapter') || strpos($editor, 'tinymce5Adapter')) {
 
             if (($result->getDataByPath('settings/menubar')) || ($result->getDataByPath('settings/toolbar')) || ($result->getDataByPath('settings/plugins'))) {
                 // do not override ui_element config (unsure if this is needed)
@@ -114,7 +114,7 @@ class ConfigPlugin
 
             $settings['plugins'] = 'advlist autolink code colorpicker directionality hr imagetools link media noneditable paste print table textcolor toc visualchars anchor charmap codesample contextmenu help image insertdatetime lists nonbreaking pagebreak preview searchreplace template textpattern visualblocks wordcount magentovariable magentowidget';
 
-            $settings['toolbar1'] = 'magentovariable magentowidget | formatselect | styleselect | fontselect | fontsizeselect | forecolor backcolor | bold italic underline strikethrough | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent';
+            $settings['toolbar1'] = 'magentovariable magentowidget | formatselect | styleselect | fontselect | fontsizeselect | lineheight | forecolor backcolor | bold italic underline strikethrough | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent';
             $settings['toolbar2'] = ' undo redo  | link anchor table charmap | image media insertdatetime | widget | searchreplace visualblocks  help | hr pagebreak';
             $settings['force_p_newlines'] = false;
 
